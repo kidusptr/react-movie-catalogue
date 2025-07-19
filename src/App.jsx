3477637e89b1abbc533c1185e8a5e05ec27b235d
Kidus Petros <kidusptr@gmail.com>
2025-07-19 23:00:30 +0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+const Card = ({ title }) => {
+  return <h2>{title}</h2>;
+};
 const App = () => {
-  return <h2>Arrow function</h2>;
+  return (
+    <div>
+      <Card title="Iron Man" />
+      <Card title="Captain America" />
+      <Card title="Thor" />
+      <Card title="Hulk" />
+    </div>
+  );
 };
 
 export default App;
