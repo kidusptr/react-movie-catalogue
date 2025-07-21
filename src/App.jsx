bea2f5b22232df5ec21d9a3b9c91403c725f8e9e
Kidus Petros <kidusptr@gmail.com>
2025-07-21 23:47:30 +0300

    state handling with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-const Card = ({ title }) => {
+import React, { useState } from "react";
+
+const Card = ({ title, hasliked, onClick }) => {
   return (
-    <div className="card">
+    <div
+      className="card"
+      style={{ backgroundColor: hasliked ? "red" : "green" }}
+      onClick={onClick}
+    >
       <h2>{title}</h2>
     </div>
   );
 };
 const App = () => {
+  const [hasliked, setHasliked] = useState(false);
+  const toggleLike = () => {
+    setHasliked(!hasliked);
+  };
   return (
     <div className="card-container">
-      <Card title="Iron Man" />
-      <Card title="Captain America" />
-      <Card title="Thor" />
-      <Card title="Hulk" />
+      <Card title="Iron Man" hasliked={hasliked} onClick={toggleLike} />
+      <Card title="Captain America" hasliked={hasliked} onClick={toggleLike} />
+      <Card title="Thor" hasliked={hasliked} onClick={toggleLike} />
+      <Card title="Hulk" hasliked={hasliked} onClick={toggleLike} />
     </div>
   );
 };
