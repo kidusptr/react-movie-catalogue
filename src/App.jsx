d1d955e45589c3a1d2ba2dc163a94169ab71c15e
Kidus Petros <kidusptr@gmail.com>
2025-07-21 23:33:21 +0300

    inline external sheet styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 const Card = ({ title }) => {
-  return <h2>{title}</h2>;
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+    </div>
+  );
 };
 const App = () => {
   return (
-    <div>
+    <div className="card-container">
       <Card title="Iron Man" />
       <Card title="Captain America" />
       <Card title="Thor" />
