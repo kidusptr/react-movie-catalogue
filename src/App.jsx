7b57e9b3a765903d46cc6b8947cf6ab05da39445
Kidus Petros <kidusptr@gmail.com>
2025-08-15 22:25:01 +0300

    working on search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-
+import Search from "./components/search";
 const App = () => {
+  // Main application component
+  const [searchTerm, setSearchTerm] = React.useState("");
   return (
     <main>
       <div className="pattern" />
       <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="hero banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll enjoy
             without the hustle
           </h1>
         </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const search = () => {
+  return <div className="text-white text-3xl">Search</div>;
+};
+
+export default search;
