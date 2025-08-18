66ee892e2bba78daf1580242eda4f3bca7add5e4
Kidus Petros <kidusptr@gmail.com>
2025-08-18 23:47:02 +0300

    getting popular movies

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,36 @@ import { useState, useEffect } from "react";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 const App = () => {
   // Main application component
   const [searchTerm, setSearchTerm] = useState("");
-  useEffect(() => {}, []);
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`,
+        API_OPTIONS
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
