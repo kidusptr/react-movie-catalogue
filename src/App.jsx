481c55bb0f90e326c13942b8c2d91ead8e2672b1
Kidus Petros <kidusptr@gmail.com>
2025-08-18 23:34:16 +0300

    preparing api calling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Search from "./components/search";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
 const App = () => {
   // Main application component
   const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {}, []);
+
   return (
     <main>
       <div className="pattern" />
