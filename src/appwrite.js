1881061fbb47716790e50364e1a6755f8c5d2582
Kidus Petros <kidusptr@gmail.com>
2025-08-29 23:04:27 +0300

    connecting to appwrite database

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,7 +1,12 @@
+import { Client, Databases } from "appwrite";
 const ProjectID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DatabaseID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const CollectionID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const Endpoint = import.meta.env.VITE_APPWRITE_ENDPOINT;
 
+const client = new Client().setEndpoint(Endpoint).setProject(ProjectID);
+
+const database = new Databases(client);
 export const updateSearchCount = (searchTerm) => {
   console.log(ProjectID, DatabaseID, CollectionID);
 };
